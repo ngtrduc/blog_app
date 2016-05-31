05b88128d8bec81181489ba68536bc5f8f383ed6
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-05-31 14:38:46 +0700

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
+
+  get 'signup' => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
