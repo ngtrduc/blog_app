3cf884e057f6a2f7d8e0ace767017cec5ba08d52
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-06-01 15:46:41 +0700

    search-bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   	if logged_in?
   		@post = current_user.posts.build
   		@comment = current_user.comments.build
-  		@feed_items = current_user.feed.paginate(page: params[:page])
+  		@feed_items = current_user.feed.paginate(page: params[:page],  :per_page => 6)
   	end
   end
 
@@ -11,5 +11,14 @@ class StaticPagesController < ApplicationController
   end
 
   def search
+  	@comment = current_user.comments.build if logged_in?
+  	if params[:search_by] == '1'
+    @posts = Post.where("title like ?", "%#{params[:search]}%").paginate(page: params[:page], :per_page => 6)
+    @users = nil
+    end
+    if params[:search_by] == '2'
+    @users=  User.where("name like ?", "%#{params[:search]}%").paginate(page: params[:page], :per_page => 6)
+    @posts=nil
+    end
   end
 end
