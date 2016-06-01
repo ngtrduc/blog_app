51d4a96af0ba2c9113326b231b90d92fcc133eda
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-06-01 14:45:02 +0700

    comment delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,15 @@ class CommentsController < ApplicationController
   	@comment = Comment.new(comment_params)
   	@comment.user_id = current_user.id
     if @comment.save
-      respond_to do |format|
-        format.html do
-          redirect_to @post
-        end
-        format.js 
-      end
+      redirect_to request.referrer || root_url
     end
   end
 
   def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+    flash[:success] = "comment deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
