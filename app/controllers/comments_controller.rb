7589c6d34323c8bed50a96ab1df52a4e9b95e276
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-06-02 11:45:37 +0700

    comment ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,30 @@ class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   
   def create
-
   	@comment = Comment.new(comment_params)
   	@comment.user_id = current_user.id
-    post = Post.find(@comment.post_id)
+    @post = Post.find(@comment.post_id)
     noti = Noti.new
     noti.noti_type = 1
     noti.post_id = @comment.post_id
-    noti.user_id = post.user_id
+    noti.user_id = @post.user_id
     noti.status = true
     if @comment.save && noti.save
-      redirect_to request.referrer || root_url
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
     end
   end
 
   def destroy
     @comment = current_user.comments.find(params[:id])
+    @post = @comment.post
     @comment.destroy
-    flash[:success] = "comment deleted"
-    redirect_to request.referrer || root_url
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.js
+    end
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
   validates :content, presence: true, length: { maximum: 140 }
 end
